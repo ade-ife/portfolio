a179301ec3d0dfe54f7fe0f2e7ad6d1f3574d6aa
Joshua Niji <jadeniji54@gmail.com>
2022-07-16 03:11:14 +0100

    loading-animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,44 @@
 // import './App.css';
-import React from 'react'
+import React,  { useState, useEffect } from 'react'
 import Header from './components/header/Header'
 import Nav from './components/nav/Nav'
 import About from './components/about/About'
 import Experience from './components/experience/Experience'
-// import Testimonials from './components/testimonials/Testimonials'
 import Contact from './components/contact/Contact'
 import Footer from './components/footer/Footer'
 
 
-
-
-
 const App = () => {
-  return (
-    <div>
-      <Header />
-      <Nav />
-      <About />
-      <Experience />
-    
-      {/* <Testimonials /> */}
-      <Contact />
-      <Footer />
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2620);
+  }, []);
 
+  return (
+
+    <div className="container">
+        {loading ? (
+          <div className="loader-container">
+           <div className='cook'> <h2>e fi si le, e je ko cook</h2> </div>
+            <div className="spinner"></div>
+          </div>
+      ) : (
+
+
+      <div>
+        <Header />
+        <Nav />
+        <About />
+        <Experience />
+      
+
+        <Contact />
+        <Footer />
+      </div>
+      )}
     </div>
   )
 }
