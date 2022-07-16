0e5276e10889f8c85855e3db094b5476058ecb52
ade-ife <37552066+ade-ife@users.noreply.github.com>
2022-07-16 03:56:51 +0100

    Revert "loading-animation"
    This reverts commit a179301ec3d0dfe54f7fe0f2e7ad6d1f3574d6aa.

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,29 @@
 // import './App.css';
-import React,  { useState, useEffect } from 'react'
+import React from 'react'
 import Header from './components/header/Header'
 import Nav from './components/nav/Nav'
 import About from './components/about/About'
 import Experience from './components/experience/Experience'
+// import Testimonials from './components/testimonials/Testimonials'
 import Contact from './components/contact/Contact'
 import Footer from './components/footer/Footer'
 
 
+
+
+
 const App = () => {
-  const [loading, setLoading] = useState(false);
-  useEffect(() => {
-    setLoading(true);
-    setTimeout(() => {
-      setLoading(false);
-    }, 2620);
-  }, []);
-
   return (
+    <div>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+    
+      {/* <Testimonials /> */}
+      <Contact />
+      <Footer />
 
-    <div className="container">
-        {loading ? (
-          <div className="loader-container">
-           <div className='cook'> <h2>e fi si le, e je ko cook</h2> </div>
-            <div className="spinner"></div>
-          </div>
-      ) : (
-
-
-      <div>
-        <Header />
-        <Nav />
-        <About />
-        <Experience />
-      
-
-        <Contact />
-        <Footer />
-      </div>
-      )}
     </div>
   )
 }
