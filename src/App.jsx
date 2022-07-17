50484efaca7a2b7fc9d1ed81d4a00936abe773bb
ade-ife <37552066+ade-ife@users.noreply.github.com>
2022-07-17 21:35:40 +0100

    animation-add

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 // import './App.css';
-import React from 'react'
+import React , { useState, useEffect } from 'react'
 import Header from './components/header/Header'
 import Nav from './components/nav/Nav'
 import About from './components/about/About'
 import Experience from './components/experience/Experience'
-// import Testimonials from './components/testimonials/Testimonials'
 import Contact from './components/contact/Contact'
 import Footer from './components/footer/Footer'
 
@@ -13,18 +12,36 @@ import Footer from './components/footer/Footer'
 
 
 const App = () => {
+
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
+
+
   return (
+    <div className="container">
+      {loading ? (
+        <div className="loader-container">
+      	  <div className="spinner"></div>
+        </div>
+      ) : (
+
     <div>
       <Header />
       <Nav />
       <About />
       <Experience />
-    
-      {/* <Testimonials /> */}
       <Contact />
       <Footer />
 
     </div>
+      )}
+    </div>
+    
   )
 }
 
